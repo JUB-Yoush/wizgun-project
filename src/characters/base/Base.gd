1852504ccc430b3e271800b2bb585804105991cb
JUB_Yoush <yoush1337@gmail.com>
2022-04-27 17:27:25 -0400

    got slide working, going to work on projectile next

--- src/characters/base/Base.gd
+++ src/characters/base/Base.gd
@@ -6,6 +6,13 @@ export (float,0,1.0) var friction = 0.2
 export (float,0,1.0) var acceleration = 0.2
 export var gravity:float = 25
 export var slash_speed:float = 200
+
+export var slide_speed:float = 600
+var slide_velocity:Vector2 = Vector2.ZERO
+export var slide_friction:float = 20
+
+
+
 const MAX_GRAVITY = 2000
 const UP = Vector2.UP
 
@@ -17,25 +24,10 @@ enum States {
 	IN_AIR,
 	AIRDODGE
 	}
-func on_ground(delta):
-	jump()
-	move(delta)
 
-func sliding(delta):
-	pass
-
-func slashing(delta):
-	pass
-
-func shooting(delta):
-	pass
-
-func in_air(delta):
-	move(delta)
-	if is_on_floor():
-		_state = States.ON_GROUND
 
 var dir = Vector2.ZERO
+var last_dir = Vector2.ZERO
 var velocity = Vector2.ZERO
 var _state = States.ON_GROUND
 
@@ -43,24 +35,55 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	update_dir()
 	match _state:
 		States.ON_GROUND:
 			on_ground(delta)
 		States.SLIDING:
-			sliding(delta)
+			var dir_at_press = last_dir
+			sliding(delta,dir_at_press)
 		States.IN_AIR:
 			in_air(delta)
 		States.SHOOTING:
 			shooting(delta)
 		States.SLASHING:
-			shooting(delta)
+			slashing(delta)
 	
-	print(_state)
+	#print(_state)
 			
 	
+# STATES ---------------------------------------------------------
+func on_ground(delta):
+	update_dir()
+	jump()
+	move(delta)
+	slide()
+
+func sliding(delta:float,passed_in_dir:Vector2):
+	velocity.x = slide_velocity.x 
+	slide_velocity.x = abs(slide_velocity.x - slide_friction) 
+	velocity.y = min(velocity.y + gravity,MAX_GRAVITY)
+	velocity.x = velocity.x * passed_in_dir.x
+	velocity = move_and_slide(velocity,UP)
+	if slide_velocity.x == 0:
+		_state = States.ON_GROUND
 	
 	
+	
+func slashing(delta):
+	pass
+
+func shooting(delta):
+	pass
+
+func in_air(delta):
+	update_dir()
+	move(delta)
+	if is_on_floor():
+		_state = States.ON_GROUND
+
+
+# COMPONENTS ---------------------------------------------------------
+
 func move(delta):
 	if dir.x != 0:
 		velocity.x = lerp(velocity.x, dir.x * speed, acceleration)
@@ -73,9 +96,13 @@ func move(delta):
 func update_dir():
 	dir.x = Input.get_action_strength("p1_right") - Input.get_action_strength("p1_left")
 	dir.y = Input.get_action_strength("p1_down") - Input.get_action_strength("p1_up")
+	if dir.x != 0:
+		last_dir.x = dir.x
+	
 	
 
 func jump():
+	#print(dir.y)
 	var is_jumping:bool = Input.is_action_just_pressed("p1_jump") and dir.y <= 0
 	if is_jumping:
 		velocity.y = -jump_str
@@ -85,6 +112,7 @@ func jump():
 		velocity.y = lerp(velocity.y, 0, 0.5)
 
 func slide():
-	if Input.is_action_just_pressed("p1_jump") and dir.y > 0:
-		pass #slide
+	if Input.is_action_just_pressed("p1_jump") and dir.y == 1:
+		slide_velocity.x = slide_speed
+		_state = States.SLIDING
 
