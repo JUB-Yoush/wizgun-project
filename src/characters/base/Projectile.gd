140a39ba5c6e72ed89724907c95ea7d24242e650
JUB_Yoush <yoush1337@gmail.com>
2022-05-04 17:58:30 -0400

    hitstop stops proj now, need to get it to stop player

--- src/characters/base/Projectile.gd
+++ src/characters/base/Projectile.gd
@@ -6,17 +6,19 @@ var duration = 0.7
 var dir:Vector2 = Vector2.ZERO
 var lifetime = 5
 export var speed:float = 10
-export var reflected_speed = 20
+export var reflected_speed = 5
 var velocity:Vector2
 var reflected:bool = false
 onready var visibilityNotif = $VisibilityNotifier2D
 onready var lifespanTimer = $LifespanTimer
 
 const PARENTS:Array = ["p1","p2"]
-
+signal start_hitstop(caller,time_scale, duration)
+signal hitstop_over
 var parent_tag:String
 # Called when the node enters the scene tree for the first time.
 func _ready(): 
+	connect("start_hitstop",get_parent(),"make_hitstop")
 	connect("body_entered",self,"on_body_entered")
 	lifespanTimer.connect("timeout",self,"on_lifespan_timeout")
 	lifespanTimer.start(lifetime)
@@ -73,10 +75,14 @@ func on_area_entered(area:Area2D):
 	print(area.name)
 	
 func reflect(body:KinematicBody2D):
+	dir = Vector2.ZERO
+	set_modulate("#ffffff")
+	emit_signal("start_hitstop",self, time_scale, duration)
+	#make_hitstop(time_scale, duration)
+	yield(get_parent(),"hitstop_over")
+	set_modulate("#10940f")
 	if parent_tag == PARENTS[1]: parent_tag = PARENTS[0]
 	elif parent_tag ==  PARENTS[0]: parent_tag = PARENTS[1]
 	dir = body.last_dir
-	speed = reflected_speed
-	set_modulate("#ffffff")
-	body.emit_signal("start_hitstop", time_scale, duration)
-	set_modulate("#10940f")
+	speed += reflected_speed
+	
--- src/stage/Main.gd
+++ src/stage/Main.gd
@@ -2,16 +2,19 @@ extends Node2D
 
 onready var respawnPositions = $RespawnPositions
 
+signal hitstop_over
+
 func get_respawn_position():
 	var respawn_position:Position2D
 	var rng = randi() % respawnPositions.get_child_count()
 	respawn_position = respawnPositions.get_child(rng)
 	return respawn_position
 
-func make_hitstop(timeScale, duration):
+func make_hitstop(caller,timeScale, duration):
+	#connect("hitstop_over",caller, "")
 	#timeScale = how slow the hitstop is (0.03)
 	#duration = how long it lasts in seconds (0.7)
 	Engine.time_scale = timeScale
 	yield(get_tree().create_timer(duration * timeScale), "timeout")
 	Engine.time_scale = 1
-	
+	emit_signal("hitstop_over")
